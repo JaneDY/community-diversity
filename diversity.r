4a639f01a81ded569e31da7c56189652d9aa151b
JaneDY <396831245@qq.com>
2019-12-30 17:39:54 +0800

    Update diversity.r

--- diversity.r
+++ diversity.r
@@ -16,7 +16,7 @@ ace <- apply(data, 1, function(x) ACE(x))
 shannon <- diversity(data, index = "shannon", base = exp(1))
 invsimpson <- diversity(data, index = "invsimpson", base = exp(1))
 
-div <- rbind(chao,ace,shannon,invsimpson)
+div <- cbind(chao,ace,shannon,invsimpson)
 write.table(div, paste0("alpha_div.",args[1]), sep="\t", row.names=T, col.names=NA, quote=F)
 
 dist <- vegdist(data, method="bray")
